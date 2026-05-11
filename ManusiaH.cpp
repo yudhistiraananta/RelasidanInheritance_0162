1ba318c5b812f539cd7a07e1c667fd5b85ad971a
yudhistiraananta <yudhistiraananta16@gmail.com>
2026-05-11 15:57:46 +0700

    Manusia 2

--- ManusiaH.cpp
+++ ManusiaH.cpp
@@ -2,3 +2,19 @@
 #define MANUSIA_H
 
 using namespace std;
+
+class manusia {
+public:
+    string name;
+    jantung varJantung;
+
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+    }
+    ~manusia() {
+        cout << name << " mati\n";
+    }
+};
+
+#endif
