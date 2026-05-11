d2a8980c807a5d8ec36411927eba972823c7f639
yudhistiraananta <yudhistiraananta16@gmail.com>
2026-05-11 15:49:42 +0700

    inheritance 2

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -19,3 +19,18 @@ public:
     }
 
 };
+
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n";
+    }
+};
