d422785be7bfcd0d247819c73a3f8ea67a1d7639
yudhistiraananta <yudhistiraananta16@gmail.com>
2026-05-11 15:28:54 +0700

    Agregasi ibu 2

--- AgregasiIbu.cpp
+++ AgregasiIbu.cpp
@@ -1,3 +1,19 @@
 #ifndef IBU_H
 #define IBU_H
 #include <vector>
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
