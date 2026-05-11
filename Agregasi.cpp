e970c7298da3808b447eb9a23a4daab308cdc8b5
yudhistiraananta <yudhistiraananta16@gmail.com>
2026-05-11 15:36:27 +0700

    Agregasi 4

--- Agregasi.cpp
+++ Agregasi.cpp
@@ -16,3 +16,16 @@ int main() { //agregasi
     varibu2->tambahAnak(varAnak3);
     varibu2->tambahAnak(varAnak1);
 
+    varIbu->cetakAnak();
+    varibu2->cetakAnak();
+
+    delete varIbu;
+    delete varibu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    system("pause");
+
+    return 0;
+}
