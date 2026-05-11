75d9526844fdb9f998fc3b42a7eaf1f271083808
yudhistiraananta <yudhistiraananta16@gmail.com>
2026-05-11 15:54:57 +0700

    Jantung 2

--- JantungH.cpp
+++ JantungH.cpp
@@ -2,3 +2,15 @@
 #define JANTUNG_H
 
 using namespace std;
+
+class jantung {
+public:
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+
+#endif
